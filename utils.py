1ecd22d52682f241f1abd8e45324472175052893
qper <ukr0305@gmail.com>
2025-03-14 13:04:01 +0200

    Added function to check sorted array

--- utils.py
+++ utils.py
@@ -9,3 +9,9 @@ def validate_partition_size(data: list, partition_count: int) -> bool:
 
 def split_array(data: list, partition_count: int):
     return np.array(data).reshape(partition_count, -1)
+
+def check_sorted_array(array: list) -> bool:
+    for i in range(len(array) - 1):
+        if array[i + 1] < array[i]:
+            return False
+    return True
