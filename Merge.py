dc85682d1b400aef2e80a62ce2c9977344cb4a77
qper <ukr0305@gmail.com>
2025-03-14 12:44:46 +0200

    Added Merge class, finished Selection sort

---
+++ Merge.py
@@ -0,0 +1,35 @@
+def merge_two_arrays(left, right):
+    merged = []
+    left_index = right_index = 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+    
+    if left_index < len(left):
+        merged.extend(left[left_index:])
+    
+    if right_index < len(right):
+        merged.extend(right[right_index:])
+    
+    return merged
+
+def merge_sorted_arrays(arrays):
+    if len(arrays) == 0:
+        return []
+    
+    if len(arrays) == 1:
+        return arrays[0]
+    
+    if len(arrays) == 2:
+        return merge_two_arrays(arrays[0], arrays[1])
+    
+    middle = len(arrays) // 2
+    left_half = merge_sorted_arrays(arrays[:middle])
+    right_half = merge_sorted_arrays(arrays[middle:])
+    
+    return merge_two_arrays(left_half, right_half)
--- SelectionSort.py
+++ SelectionSort.py
@@ -1,3 +1,7 @@
+import sys
+from utils import validate_array_division, split_array
+from Merge import merge_sorted_arrays
+
 def selection_sort(array):
     length = len(array)
     
@@ -9,4 +13,15 @@ def selection_sort(array):
                 
         if min_idx != i:
             array[i], array[min_idx] = array[min_idx], array[i]
+
+def sort(array, num_chunks):
+    if not validate_array_division(array, num_chunks):
+        sys.exit(1)
+    
+    chunks = split_array(array, num_chunks)
+    
+    for chunk in chunks:
+        selection_sort(chunk)
+    
+    return merge_sorted_arrays(chunks)
 
