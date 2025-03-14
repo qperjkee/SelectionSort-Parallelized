5edb21e95fcc1310651d34fda79807f5d314b6d0
qper <ukr0305@gmail.com>
2025-03-14 12:07:06 +0200

    Added Car class to be sorted in future

---
+++ Car.py
@@ -0,0 +1,52 @@
+from random import randint, randrange
+
+class Car:
+    def __init__(self, engine_power=None, max_speed=None, fuel_consumption=None) -> None:
+        self._engine_power = engine_power if engine_power else randint(50, 1000)  # Engine power (hp)
+        self._max_speed = max_speed if max_speed else randrange(100, 401)  # Max speed (km/h)
+        self._fuel_consumption = fuel_consumption if fuel_consumption else randrange(30, 301) / 10  # Fuel consumption (l/100 km)
+
+    def __str__(self) -> str:
+        return (f"Car: {self._engine_power} hp, {self._max_speed} km/h max speed, "
+                f"{self._fuel_consumption:.1f} l/100 km fuel consumption")
+
+    def __repr__(self) -> str:
+        return (f"Car(engine_power={self._engine_power}, max_speed={self._max_speed}, "
+                f"fuel_consumption={self._fuel_consumption:.1f})")
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Car):
+            return (self._engine_power == other._engine_power and
+                    self._max_speed == other._max_speed and
+                    self._fuel_consumption == other._fuel_consumption)
+        return False
+
+    def __lt__(self, other) -> bool:
+        if not isinstance(other, Car):
+            return False
+        if self._engine_power == other._engine_power:
+            return self._max_speed < other._max_speed
+        elif self._max_speed == other._max_speed:
+            return self._fuel_consumption > other._fuel_consumption
+        else:
+            return self._engine_power < other._engine_power
+
+    def __le__(self, other) -> bool:
+        if not isinstance(other, Car):
+            return False
+        if self._engine_power == other._engine_power:
+            return self._max_speed <= other._max_speed
+        elif self._max_speed == other._max_speed:
+            return self._fuel_consumption <= other._fuel_consumption
+        else:
+            return self._engine_power <= other._engine_power
+
+    def __gt__(self, other) -> bool:
+        if not isinstance(other, Car):
+            return False
+        if self._engine_power == other._engine_power:
+            return self._max_speed > other._max_speed
+        elif self._max_speed == other._max_speed:
+            return self._fuel_consumption < other._fuel_consumption
+        else:
+            return self._engine_power > other._engine_power
